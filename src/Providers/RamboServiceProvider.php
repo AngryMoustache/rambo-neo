cecbc74b2c162a1bcb7d4452c39129e69fb88522
AngryMoustache <sander.vandamme11@gmail.com>
2022-04-16 13:55:32 +0200

    Barebones

---
+++ src/Providers/RamboServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AngryMoustache\Rambo\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RamboServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
