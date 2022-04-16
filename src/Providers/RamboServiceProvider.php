1d4e40a51bba98a57c08551e70868694ad8d25d5
AngryMoustache <sander.vandamme11@gmail.com>
2022-04-16 14:10:22 +0200

    Provider

--- src/Providers/RamboServiceProvider.php
+++ src/Providers/RamboServiceProvider.php
@@ -8,7 +8,16 @@ class RamboServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->publishes([
+            __DIR__ . '/../../config/rambo.php' => config_path('rambo.php'),
+        ], 'rambo-config');
 
+        $this->publishes([
+            __DIR__ . '/../../resources/assets' => public_path('vendor/rambo'),
+        ], 'rambo-assets');
+
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
     }
 
     public function register()
